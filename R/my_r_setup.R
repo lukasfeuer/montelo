9b1671b5d8e9d4451872d750beb53b40fe7dd370
Lukas Feuer <feuerlukas@web.de>
2022-04-21 08:52:01 +0200

    changes in my_r_setup() and Description

--- R/my_r_setup.R
+++ R/my_r_setup.R
@@ -24,58 +24,94 @@ my_r_setup <- function() {
       "PerformanceAnalytics",
       "airports",
       "apaTables",
+      "bayesplot",
+      "blastula",
+      "blogdown",
       "broom",
+      #"Cairo",
       "caret",
       "corrplot",
+      "countrycode",
       "dashboardthemes",
       "dtplyr",
       "usethis",
       "eurostat",
       "extrafont",
       "flexdashboard",
+      "fontawesome",
+      "fs",
       "gapminder",
+      "GGally",
       "gganimate",
+      "ggrepel",
       "ggridges",
       "ggthemes",
       "glue",
       "haven",
       "here",
+      "Hmisc",
+      "htmlTable",
       "htmltools",
       "htmlwidgets",
       "httr",
+      "imputeTS",
       "jsonlite",
       "ISOweek",
       "janitor",
+      "kableExtra",
+      #"keras",
+      "knitr",
       "lavaan",
       "leaflet",
       "lm.beta",
       "lme4",
+      "lobstr",
       "lubridate",
+      #"magick",
       "magrittr",
       "manipulateWidget",
       "markdown",
+      "MVN",
+      "nycflights13",
       "officer",
       "packrat",
+      "pastecs",
+      "pdftools",
+      "profvis",
       "psych",
+      "quantmod",
+      "questionr",
       "QuantPsyc",
       "RColorBrewer",
       "rhandsontable",
       "rio",
       "rmarkdown",
+      "roxygen2",
       "rsthemes",
       "rstudioapi",
       "rvest",
+      "sass",
       "scales",
       "shinydashboard",
       "shinyjs",
       "showtext",
       "slider",
       "svglite",
+      #"tensorflow",
+      #"tesseract",
+      "testthat",
       "timevis",
-      "viridis",
+      #"tinytex",
+      "usethis",
+      #"viridis",
+      "viridisLite",
       "vroom",
       "waldo",
       "webshot",
+      "whisker",
+      #"wordcloud2",
+      "XML",
+      "xml2",
       "taskscheduleR"
     )
   )
@@ -86,4 +122,22 @@ my_r_setup <- function() {
   #rstudioapi::applyTheme("Night Owl {rsthemes}")
   #rstudioapi::applyTheme("Material {rsthemes}")
   #rstudioapi::applyTheme("Material Ocean {rsthemes} ")
+
+  message(paste0(
+    "---- Please also visit and install: ---------------",
+    "\n https://github.com/nvbn/thefuck",
+    "\n test"
+  ))
 }
+
+
+
+# TODO
+#   log for failed package installations
+#   --> write a custom function that maps over the list
+#   --> try?
+#
+#   print(list("packages installed successful),
+#         list("packages failed to install + errormessages"))
+#         --> always install dependencies
+#
